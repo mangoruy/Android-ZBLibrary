b35130810571e0969aae90f413503492f22fd4b1
yang <yangfanfancy@163.com>
2019-06-26 09:14:13 +0800

    6/26 6月份第一次进行开发 准备除了login页面外全部采用webview 减少开发时间

---
+++ app/src/main/java/zblibrary/demo/DEMO/DemoLoginActivity.java
@@ -0,0 +1,20 @@
+package zblibrary.demo.DEMO;
+
+import zuo.biao.library.base.BaseActivity;
+
+public class DemoLoginActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+}
